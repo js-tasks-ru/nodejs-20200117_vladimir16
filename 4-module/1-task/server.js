da24bcc4d17ee71db453296679acae9b6dbaf158
Vladimir Tertyshnikov <vladimir@glamo24.ru>
2020-01-31 11:05:56 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,22 +1,32 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+
+  function sendStatusCode(statusCode) {
+    res.statusCode = statusCode;
+    res.end();
+  }
+  
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-
+      if (/[^\w\.\- ]/.test(pathname)) sendStatusCode(400);
+      else 
+        fs.createReadStream(filepath)
+          .on("error", () => {res.statusCode = 404; res.end();})
+          .pipe(res);
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      sendStatusCode(501);
   }
 });
 
