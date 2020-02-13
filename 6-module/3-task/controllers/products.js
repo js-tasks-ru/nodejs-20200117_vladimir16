1a09f19dbb4d12356cd4f3ba7eec4d3599f23a0c
VladimirTertyshnikov <59862303+VladimirTertyshnikov@users.noreply.github.com>
2020-02-13 20:52:22 +0300

    add solution for 6-module 3-task 2-release

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -2,7 +2,6 @@ const Product = require('../models/Product');
 const mapProduct = require('../mappers/product');
 
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  console.log('query', ctx.query.query);
   if (ctx.query.query) {
     const products = await Product.find({$text: {$search: ctx.query.query}}, {score: {$meta: "textScore" }}).sort({score: {$meta: "textScore" }});
     //const products = await Product.find({$text: {$search: ctx.query.query}});
