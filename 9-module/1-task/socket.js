f3680ee64538ffe2dbf61cba71e9549abca94167
VladimirTertyshnikov <59862303+VladimirTertyshnikov@users.noreply.github.com>
2020-02-21 16:18:13 +0300

    add solution for 9-module 1-task

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -1,17 +1,31 @@
 const socketIO = require('socket.io');
 
 const Session = require('./models/Session');
+const User = require('./models/User');
 const Message = require('./models/Message');
 
 function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
-    next();
+    const token = socket.handshake.query.token;
+    if (!token) return next(new Error('anonymous sessions are not allowed'));
+    const session = await Session.findOne({token: token});
+    if (!session) return next(new Error('wrong or expired session token'));
+    socket.user = await User.findById(session.user);
+    return next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      const message = new Message({
+        date: new Date(),
+        text: msg,
+        chat: socket.user.id,
+        user: socket.user.displayName
+      });
+      message.save({validateBeforeSave: true});
+    });
   });
 
   return io;
